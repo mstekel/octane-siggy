7115d2eb98103f245cb8841f342d3bae0a39d59e
stekel <moshe.stekel@microfocus.com>
2019-02-12 03:09:13 +0200

    Refactor: 1. Don't use conv inside intentHandlers.

--- functions/src/fulfillment.ts
+++ functions/src/fulfillment.ts
@@ -22,12 +22,6 @@ const octane = new Octane({
     routesConfig: routesConfig,
 });
 
-const handleError = (err, conv) => {
-    console.error(err);
-    conv.ask('Error occurred: ' + err.message);
-    conv.ask('See firebase logs for more details.')
-};
-
 const host = octane.config.host || octane.constants.host;
 const protocol = octane.config.protocol || octane.constants.protocol || 'http';
 const port = octane.config.port || (protocol === 'https' ? 443 : 80);
@@ -52,7 +46,7 @@ const getSignOutUrl = () => {
 
 const cookieSuffix = "path=/; domain=.almoctane.com; Secure;";
 
-const loginWithOctane = conv => new Promise((resolve, reject) => {
+const loginWithOctane = () => new Promise((resolve, reject) => {
     request.get(
         {
             url: getGrantTokenUrl(),
@@ -63,30 +57,33 @@ const loginWithOctane = conv => new Promise((resolve, reject) => {
                 reject(error);
             } else {
                 const responseBody = JSON.parse(body);
-                conv.user.storage.octaneUserId = responseBody.identifier;
-                conv.ask('Login With Octane');
-                conv.ask(new BasicCard({
-                    title: 'Login With Octane',
-                    text: 'Use the button below to open Octane Log-in Screen',
-                    buttons: [
-                        new Button({
+                resolve({
+                    octaneUserId: responseBody.identifier,
+                    questions: [
+                        'Login With Octane',
+                        new BasicCard({
                             title: 'Login With Octane',
-                            action: new OpenUrlAction({
-                                url: responseBody.authentication_url
-                            })
+                            text: 'Use the button below to open Octane Log-in Screen',
+                            buttons: [
+                                new Button({
+                                    title: 'Login With Octane',
+                                    action: new OpenUrlAction({
+                                        url: responseBody.authentication_url
+                                    })
+                                })
+                            ]
                         })
                     ]
-                }));
-                resolve();
+                });
             }
         });
 });
 
-const authenticate = (conv) => new Promise((resolve, reject) => {
+const authenticate = (octaneUserId) => new Promise((resolve, reject) => {
     request.post(
         {
             url: getGrantTokenUrl(),
-            json: {'identifier': conv.user.storage.octaneUserId}
+            json: {'identifier': octaneUserId}
         },
         (error, response, body) => {
             if (response.statusCode !== 200) {
@@ -141,174 +138,185 @@ const getLastRunStatusByPipelineName = pipeline => new Promise((resolve, reject)
     });
 });
 
-const getIntentHandler = conv => {
-    const intentMap = {
-        'welcome_intent': username => new Promise((resolve, reject) => {
-            conv.ask('Hi, I\'m Octane Siggy. Talk to me!');
-            resolve();
-        }),
-        'my_top_priority_items': username => new Promise((resolve, reject) => {
-            const userQuery = Query.field('name').equal(username);
-            const query = Query.field('owner').equal(userQuery)
-                .and().field('subtype').inComparison(['defect', 'story'])
-                .and().field('phase').equal(Query.field('logical_name')
-                    .inComparison(['phase.defect.new', 'phase.defect.opened', 'phase.story.new', 'phase.story.inprogress']));
-            octane.workItems.getAll({
-                limit: 5,
-                query: query,
-                order_by: 'rank'
-            }, (ex, items) => {
-                if (ex) {
-                    reject(ex);
-                } else {
-                    const rows = [];
-                    let defectCount = 0,
-                        storyCount = 0;
-                    for (const i in items) {
-                        if (items[i].subtype) {
-                            if (items[i].subtype === 'defect')
-                                defectCount++;
-                            else if (items[i].subtype === 'story')
-                                storyCount++;
-                            rows.push([items[i].subtype + ' #' + items[i].id + ': ' + items[i].name]);
-                        }
-                    }
-                    let message = 'You have ' + items.length + ' most top items';
-                    if (defectCount > 0 && storyCount > 0) {
-                        message += ': ' + defectCount + ' defects and ' + storyCount + ' user stories.'
-                    } else if (defectCount > 0) {
-                        message += ': ' + defectCount + ' defects.';
-                    } else if (storyCount > 0) {
-                        message += ': ' + storyCount + ' user stories.'
-                    } else {
-                        message += '.';
-                    }
-
-                    if (rows.length > 0) {
-                        conv.ask(message + ' Below is the list: ');
-                        conv.ask(new Table({
-                            dividers: true,
-                            columns: ['Name'],
-                            rows: rows
-                        }));
-                        resolve();
-                    } else {
-                        conv.ask(message);
-                        resolve();
+const intentMap = {
+    'welcome_intent': username => new Promise((resolve, reject) => {
+        resolve({questions: ['Hi, I\'m Octane Siggy. Talk to me!']});
+    }),
+    'my_top_priority_items': username => new Promise((resolve, reject) => {
+        const userQuery = Query.field('name').equal(username);
+        const query = Query.field('owner').equal(userQuery)
+            .and().field('subtype').inComparison(['defect', 'story'])
+            .and().field('phase').equal(Query.field('logical_name')
+                .inComparison(['phase.defect.new', 'phase.defect.opened', 'phase.story.new', 'phase.story.inprogress']));
+        octane.workItems.getAll({
+            limit: 5,
+            query: query,
+            order_by: 'rank'
+        }, (ex, items) => {
+            if (ex) {
+                reject(ex);
+            } else {
+                const rows = [];
+                let defectCount = 0,
+                    storyCount = 0;
+                for (const i in items) {
+                    if (items[i].subtype) {
+                        if (items[i].subtype === 'defect')
+                            defectCount++;
+                        else if (items[i].subtype === 'story')
+                            storyCount++;
+                        rows.push([items[i].subtype + ' #' + items[i].id + ': ' + items[i].name]);
                     }
                 }
-            });
-        }),
-        'did_i_break_the_build': username => new Promise((resolve, reject) => {
-            const query1 = Query.field('subtype').inComparison(['gherkin_automated_run', 'run_automated'])
-                .and().field('latest_pipeline_run').equal(true).and().field('merged_on_it').notEqual(Query.NONE);
-            octane.runs.getAll({
-                query: query1
-            }, (ex, items) => {
-                if (ex) {
-                    reject(ex);
+                let message = 'You have ' + items.length + ' most top items';
+                if (defectCount > 0 && storyCount > 0) {
+                    message += ': ' + defectCount + ' defects and ' + storyCount + ' user stories.'
+                } else if (defectCount > 0) {
+                    message += ': ' + defectCount + ' defects.';
+                } else if (storyCount > 0) {
+                    message += ': ' + storyCount + ' user stories.'
                 } else {
-                    let youBrokeTheBuild = false;
-                    const userIds = [];
-                    for (const i in items) {
-                        if (items[i].merged_on_it) {
-                            youBrokeTheBuild = youBrokeTheBuild || username === items[i].merged_on_it.name;
-                            userIds.push(items[i].merged_on_it.id);
-                        }
+                    message += '.';
+                }
+
+                if (rows.length > 0) {
+                    resolve({
+                        questions: [
+                            message + ' Below is the list: ',
+                            new Table({
+                                dividers: true,
+                                columns: ['Name'],
+                                rows: rows
+                            })
+                        ]
+                    });
+                } else {
+                    resolve({questions: [message]});
+                }
+            }
+        });
+    }),
+    'did_i_break_the_build': username => new Promise((resolve, reject) => {
+        const query1 = Query.field('subtype').inComparison(['gherkin_automated_run', 'run_automated'])
+            .and().field('latest_pipeline_run').equal(true).and().field('merged_on_it').notEqual(Query.NONE);
+        octane.runs.getAll({
+            query: query1
+        }, (ex, items) => {
+            if (ex) {
+                reject(ex);
+            } else {
+                let youBrokeTheBuild = false;
+                const userIds = [];
+                for (const i in items) {
+                    if (items[i].merged_on_it) {
+                        youBrokeTheBuild = youBrokeTheBuild || username === items[i].merged_on_it.name;
+                        userIds.push(items[i].merged_on_it.id);
                     }
-                    let message = 'You have' + (youBrokeTheBuild ? ' broken' : 'n\'t broken') + ' the build.';
-                    if (userIds.length > 0) {
-                        const query2 = Query.field('id').inComparison(userIds);
-                        octane.workspaceUsers.getAll({
-                            query: query2
-                        }, (ex1, users) => {
-                            if (ex1) {
-                                reject(ex1);
-                            } else {
-                                const rows = [];
-                                for (const j in users) {
-                                    rows.push([users[j].name]);
-                                }
-                                if (rows.length > 0) {
-                                    message += 'Below is the list of people who have broken the build: ';
-                                    conv.ask(message);
-                                    conv.ask(new Table({
-                                        dividers: true,
-                                        columns: ['Breaker\'s Email'],
-                                        rows: rows
-                                    }));
-                                    resolve();
-                                } else {
-                                    conv.ask(message);
-                                    resolve();
-                                }
+                }
+                let message = 'You have' + (youBrokeTheBuild ? ' broken' : 'n\'t broken') + ' the build.';
+                if (userIds.length > 0) {
+                    const query2 = Query.field('id').inComparison(userIds);
+                    octane.workspaceUsers.getAll({
+                        query: query2
+                    }, (ex1, users) => {
+                        if (ex1) {
+                            reject(ex1);
+                        } else {
+                            const rows = [];
+                            for (const j in users) {
+                                rows.push([users[j].name]);
                             }
-                        });
-                    } else {
-                        message += ' No build breakers were found at all.';
-                        conv.ask(message);
-                        resolve();
-                    }
+                            if (rows.length > 0) {
+                                message += 'Below is the list of people who have broken the build: ';
+                                resolve({
+                                    questions: [
+                                        message,
+                                        new Table({
+                                            dividers: true,
+                                            columns: ['Breaker\'s Email'],
+                                            rows: rows
+                                        })
+                                    ]
+                                });
+                            } else {
+                                resolve({questions: [message]});
+                            }
+                        }
+                    });
+                } else {
+                    message += ' No build breakers were found at all.';
+                    resolve({questions: [message]});
                 }
+            }
+        });
+    }),
+    'what_is_the_build_status': username => new Promise((resolve, reject) => {
+        Promise.all([
+            getLastRunStatusByPipelineName({name: 'ALM Octane Quick Master', shortName: 'Quick'}),
+            getLastRunStatusByPipelineName({name: 'ALM Octane Full Master', shortName: 'Full'}),
+            getLastRunStatusByPipelineName({name: 'MQM Root OP Nightly Master', shortName: 'Nightly'})
+        ]).then(data => {
+            let message = '';
+            data.forEach(text => {
+                message = message.concat(JSON.stringify(text) + '.\n');
             });
-        }),
-        'what_is_the_build_status': username => new Promise((resolve, reject) => {
-            Promise.all([
-                getLastRunStatusByPipelineName({name: 'ALM Octane Quick Master', shortName: 'Quick'}),
-                getLastRunStatusByPipelineName({name: 'ALM Octane Full Master', shortName: 'Full'}),
-                getLastRunStatusByPipelineName({name: 'MQM Root OP Nightly Master', shortName: 'Nightly'})
-            ]).then(data => {
-                let message = '';
-                data.forEach(text => {
-                    message = message.concat(JSON.stringify(text) + '.\n');
-                });
-                conv.ask(message);
-                resolve();
-            }).catch(reject);
-        }),
-        'logout': username => new Promise((resolve, reject) => {
-            conv.user.storage.octaneUserId = null;
-            conv.ask('You have been logged out from Octane Siggy. ' +
-                'Optionally use the card below if you want to logout your browser from Octane');
-            conv.ask(new BasicCard({
-                title: 'Logout From Octane',
-                text: 'Use the button below to logout from Octane',
-                buttons: [
-                    new Button({
-                        title: 'Logout From Octane',
-                        action: new OpenUrlAction({
-                            url: getSignOutUrl()
+            resolve({questions: [message]});
+        }).catch(reject);
+    }),
+    'logout': username => new Promise((resolve, reject) => {
+        resolve({
+            octaneUserId: null,
+            questions: [
+                'You have been logged out from Octane Siggy. ' +
+                'Optionally use the card below if you want to logout your browser from Octane',
+                new BasicCard({
+                    title: 'Logout From Octane',
+                    text: 'Use the button below to logout from Octane',
+                    buttons: [
+                        new Button({
+                            title: 'Logout From Octane',
+                            action: new OpenUrlAction({
+                                url: getSignOutUrl()
+                            })
                         })
-                    })
-                ]
-            }));
-            resolve();
-        }),
-        'who_am_i': username => new Promise((resolve, reject) => {
-            conv.ask('Your username is ' + username);
-            resolve();
-        })
-    };
-
-    return intentMap[conv.intent] || (username => new Promise((resolve, reject) => {
-        conv.ask('Octane doesn\'t support your request yet. Please ask Daniel Finkelstein for more budget.');
-        resolve();
-    }));
-
+                    ]
+                })
+            ]
+        });
+    }),
+    'who_am_i': username => new Promise((resolve, reject) => {
+        resolve({questions: ['Your username is ' + username]});
+    })
 };
 
 app.fallback(conv => {
+    const handleError = (err) => {
+        console.error(err);
+        conv.ask('Error occurred: ' + err.message);
+        conv.ask('See firebase logs for more details.')
+    };
+
     try {
+        const handleQuestions = data => {
+            if (data.hasOwnProperty('octaneUserId')) {
+                conv.user.storage.octaneUserId = data.octaneUserId;
+            }
+            data.questions && data.questions.forEach(q => conv.ask(q));
+        };
+
         if (!conv.user.storage.octaneUserId) {
-            return loginWithOctane(conv).catch(ex => handleError(ex, conv));
+            return loginWithOctane().then(handleQuestions).catch(handleError);
         } else {
-            return authenticate(conv).then(username => username ?
-                new Promise(resolve=>resolve(username)).then(getIntentHandler(conv)) :
-                loginWithOctane(conv)
-            ).catch(ex => handleError(ex, conv));
+            const intentHandler = intentMap[conv.intent] || (username => new Promise((resolve, reject) => {
+                resolve({questions: ['Octane doesn\'t support your request yet. Please ask Daniel Finkelstein for more budget.']});
+            }));
+            return authenticate(conv.user.storage.octaneUserId).then(username => username ?
+                new Promise(resolve => resolve(username)).then(intentHandler).then(handleQuestions) :
+                loginWithOctane().then(handleQuestions)
+            ).catch(handleError);
         }
     } catch (ex) {
-        handleError(ex, conv);
+        handleError(ex);
         return null;
     }
 });
